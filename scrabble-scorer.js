f27e119265cfb8f497e9451021064f7cc76042d7
Siri15k <sridevi15k@gmail.com>
2021-02-27 02:15:38 +0000

    Scrabble Scorer

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,6 +12,8 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -21,38 +23,124 @@ function oldScrabbleScorer(word) {
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			letterPoints += Number(pointValue); 
+      //= `Points for '${word[i]}': ${pointValue}\n`
 		 }
  
 	  }
 	}
 	return letterPoints;
  }
+ 
+ let simpleScorer = {
+  1 : ['A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+};
+function simpleScore(word) {
+  word = word.toUpperCase();
+  let wordPoints = 0;
+  for (let i = 0; i < word.length; i++) {
+    if(simpleScorer[1].includes(word[i])) {
+      wordPoints ++; 
+    }
+  }
+  return wordPoints;
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+function initialPrompt(prompt) {
+   let scrabbleWord = input.question("Let's play some scrabble! Enter a word to score:");
+   console.log(scorerPrompt(scrabbleWord));
+   //console.log("Let's play some scrabble! Enter a word:");
+   //console.log(oldScrabbleScorer(scrabbleWord));
+
+   //console.log("Word points for: " + simpleScore(scrabbleWord));
+
+   //console.log(vowelBonusScore(scrabbleWord));
 };
 
-let simpleScore;
+let vowelBonusScorer = {
+  3 : ['A','E','I','O','U'],
+  1 : ['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+};
 
-let vowelBonusScore;
+function vowelBonusScore(word) {
+  word = word.toUpperCase();
+  let vowelConsonantValue = 0;
+  for (i = 0; i < word.length; i++) {
+    for (const letterValue in vowelBonusScorer) {
+      if(vowelBonusScorer[letterValue].includes(word[i])){
+        vowelConsonantValue += Number(letterValue);
+      //= `Points for '${word[i]}': ${letterValue}\n`; 
+      }
+    }
+  }
+  return vowelConsonantValue;
+}
 
-let scrabbleScore;
+function scrabbleScore(word) {
+	word = word.toUpperCase();
+	let letterPoints = 0;
+  
+	for (let i = 0; i < word.length; i++) {
+			letterPoints += Number(newPointStructure[word[i]]);
+	}
+	
+	return letterPoints;
+}
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  {
+  name: "Simple Score",
+  description: "Each letter is worth 1 point.",
+  scorerFunction: simpleScore
+  },
+  {
+  name: "Bonus Vowels",
+  description: "Vowels are 3 pts, consonants are 1 pt.",
+  scorerFunction: vowelBonusScore
+  },
+  {
+  name: "Scrabble",
+  description: "The traditional scoring algorithm.",
+  scorerFunction: scrabbleScore
+  }
+];
 
-function scorerPrompt() {}
+function scorerPrompt(word) {
+  console.log('Which scoring algorithm would you like to use?');
 
-function transform() {};
+    for (let i = 0; i < scoringAlgorithms.length; i++){
+       console.log(`${i} - ${scoringAlgorithms[i].description}`);
+    }
+    
+     let methodNumber = input.question('Enter 0, 1, or 2: ');
+     while (isNaN(methodNumber) || methodNumber < 0 || methodNumber > 2 ){
+      methodNumber = input.question('Please enter a valid number: ');
+     } 
 
-let newPointStructure;
+     console.log(`Score for ${word}: ${scoringAlgorithms[Number(methodNumber)].scorerFunction(word)}`);
+
+}
+
+function transform(oldPointStructure) {
+  let newStructure = {};
+  for (const keyValue in oldPointStructure) {
+    for (let i = 0; i < oldPointStructure[keyValue].length; i++) {
+       newStructure[oldPointStructure[keyValue][i].toUpperCase()] = Number(keyValue);
+    }
+}
+  newStructure[' '] = 0; 
+  return newStructure;
+
+};
+
+let newPointStructure = transform(oldPointStructure);
+
 
 function runProgram() {
    initialPrompt();
-   
 }
 
 // Don't write any code below this line //
